c8e2a1c1553aeffb5b9f2c3ddec23aa7d81ee883
ZehMatt <m.moninger.h@gmail.com>
2018-04-13 12:46:17 +0200

    Style fixes: Remove some tabs

--- gamemode/sv_resource.lua
+++ gamemode/sv_resource.lua
@@ -10,25 +10,25 @@ function GM:InitializeResources()
 end
 
 function GM:AddResourceDir(dir)
-		local resourceDir = "lambda/content"
-		local foundDir = false
-		local files, folders = file.Find("gamemodes/" .. resourceDir .. "/" .. dir .. "/*", "GAME")
+	local resourceDir = "lambda/content"
+	local foundDir = false
+	local files, folders = file.Find("gamemodes/" .. resourceDir .. "/" .. dir .. "/*", "GAME")
 
-		for k, v in pairs(files) do
-			local f = dir .. "/" .. v
-			DbgPrint("Added: " .. f)
-			resource.AddFile(f)
-		end
+	for k, v in pairs(files) do
+		local f = dir .. "/" .. v
+		DbgPrint("Added: " .. f)
+		resource.AddFile(f)
+	end
 
-		for k, v in pairs(folders) do
-			local f = dir .. "/" .. v
-			if file.IsDir(resourceDir .. "/" .. f, "LUA") then
-				foundDir = true
-				self:AddResourceDir(f)
-			end
+	for k, v in pairs(folders) do
+		local f = dir .. "/" .. v
+		if file.IsDir(resourceDir .. "/" .. f, "LUA") then
+			foundDir = true
+			self:AddResourceDir(f)
 		end
+	end
 
-		if !foundDir then
-			DbgPrint("Directory " .. dir .. " was added successfully")
-		end
+	if !foundDir then
+		DbgPrint("Directory " .. dir .. " was added successfully")
+	end
 end
