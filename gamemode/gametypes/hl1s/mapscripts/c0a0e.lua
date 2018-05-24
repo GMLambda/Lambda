1235bc227e54365ad204d43de5817147e6030385
Matt <m.moninger.h@gmail.com>
2018-05-24 07:59:53 +0200

    Add c0a0e mapscript.

--- gamemode/gametypes/hl1s/mapscripts/c0a0e.lua
+++ gamemode/gametypes/hl1s/mapscripts/c0a0e.lua
@@ -29,7 +29,7 @@ MAPSCRIPT.EntityFilterByClass =
 
 MAPSCRIPT.EntityFilterByName =
 {
-	--["spawnitems_template"] = true,
+	["changetoc1a0mm"] = true,
 }
 
 function MAPSCRIPT:Init()
@@ -38,6 +38,39 @@ end
 function MAPSCRIPT:PostInit()
 
 	if SERVER then
+
+		-- Block.
+		local blockade = ents.Create("func_brush")
+		blockade:SetPos(Vector(-2120, -594.165466, -200))
+		blockade:SetModel("*15")
+		blockade:SetKeyValue("spawnflags", "2")
+		blockade:SetKeyValue("Solidity", "0")
+		blockade:SetKeyValue("solidbsp", "0")
+		blockade:SetKeyValue("StartDisabled", "1")
+		blockade:Spawn()
+		blockade:SetName("lambda_pclip1")
+
+		local doorTrigger = ents.Create("trigger_once")
+		doorTrigger:Fire("AddOutput", "OnTrigger lambda_pclip1,Enable,,0,-1")
+		doorTrigger:Fire("AddOutput", "OnTrigger doors,Toggle,,0,-1")
+		doorTrigger:SetKeyValue("teamwait", "1")
+		doorTrigger:SetupTrigger(
+			Vector(-2405.352295, -588.131409, -252.968750),
+			Angle(0,0,0),
+			Vector(-180, -80, 0),
+			Vector(200, 80, 120)
+		)
+
+		-- Lazy valve, make sure he exists.
+		local barney = ents.FindFirstByName("barney1")
+		if not IsValid(barney) then
+			barney = ents.Create("monster_barney")
+			barney:SetName("barney1")
+			barney:SetPos(Vector(-2008.000000, -472.000000, -252.968750))
+			barney:SetAngles(Angle(0, 320, 0))
+			barney:SetModel("models/hl1bar.mdl")
+			barney:Spawn()
+		end 
 
 	end
 
