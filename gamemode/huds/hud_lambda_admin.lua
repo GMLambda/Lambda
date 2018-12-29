2e045bf77a3aac204c29cec9dd2f57d5baf8fcbf
knoxed <knoxed3@gmail.com>
2018-12-29 12:32:25 +0100

    Difficulty fixes No respawn option

--- gamemode/huds/hud_lambda_admin.lua
+++ gamemode/huds/hud_lambda_admin.lua
@@ -6,12 +6,14 @@ Derma_Install_Convar_Functions(PANEL)
 function PANEL:Init()
 
 	self.Settings = GAMEMODE:GetGameTypeData("Settings") or {}
-	local y = 5
-	local n = 0
+	local y, x, n = 5, 5, 0
 
 	for k, v in pairs(self.Settings) do
 		if v.value_type == "int" or v.value_type == "float" and v.Category == "SERVER" then
-			self:AddIntOption(y, k, v)
+			if v.extra and v.extra.value_type == "bool" then
+				self:AddCheckOption(x + 130, y + 6, k, v)
+			end
+			self:AddIntOption(x, y, k, v)
 			y = y + 25
 			n = n + 1
 		end
@@ -19,53 +21,55 @@ function PANEL:Init()
 	local _y = (25 * n) + 10
 	for k, v in pairs(self.Settings) do
 		if v.value_type == "bool" and v.Category == "SERVER" then
-			self:AddCheckOption(_y, k, v)
+			self:AddCheckOption(x, _y, k, v)
 			_y = _y + 20
 			n = n + 1
 		end
 	end
 	for k, v in pairs(self.Settings) do
 		if v.value_type == "string"  and v.Category == "SERVER" then
-			self:AddComboOption(_y, k, v)
+			self:AddComboOption(x, _y, k, v)
 			_y = _y + 20
 		end
 	end
 
 end
 
-function PANEL:AddIntOption(y, id, tbl)
+function PANEL:AddIntOption(x, y, id, tbl)
 
 	local w, h = 40, 20
-	self.numw = self:Add("DNumberWang")
-	self.numw:SetPos(5, 5 + y)
-	self.numw:SetSize(w, h)
-	self.numw:SetCursorColor(colWHITE)
-	self.numw:SetMinMax(-1, tbl.maxv)
-	self.numw:SetValue(tbl.value)
-	self.numw:SetVisible(true)
+	self.Settings[id].numw = self:Add("DNumberWang")
+	self.Settings[id].numw:SetPos(x, 5 + y)
+	self.Settings[id].numw:SetSize(w, h)
+	self.Settings[id].numw:SetCursorColor(colWHITE)
+	self.Settings[id].numw:SetMinMax(-1, tbl.maxv)
+	self.Settings[id].numw:SetValue(tbl.value)
+	self.Settings[id].numw:SetVisible(true)
 
-	self.numw.lbl = self:Add("DLabel")
-	self.numw.lbl:SetPos(w + 10, y + 7)
-	self.numw.lbl:SetTextColor(colWHITE)
-	self.numw.lbl:SetText(tbl.info)
-	self.numw.lbl:SizeToContents()
-	self.numw.lbl:SetVisible(true)
+	self.Settings[id].numw.lbl = self:Add("DLabel")
+	self.Settings[id].numw.lbl:SetPos(w + 10, y + 7)
+	self.Settings[id].numw.lbl:SetTextColor(colWHITE)
+	self.Settings[id].numw.lbl:SetText(tbl.info)
+	self.Settings[id].numw.lbl:SizeToContents()
+	self.Settings[id].numw.lbl:SetVisible(true)
 
 	local function Update(id, v)
 		self.Settings[id].value = tonumber(v)
+		if tbl.extra then
+			self.Settings[id].extra.cached = tonumber(v)
+		end
 	end
-
-	function self.numw:OnValueChanged(v)
+	self.Settings[id].numw.OnValueChanged = function(self, v)
 		GAMEMODE:ChangeAdminConfiguration(id, v)
 		Update(id, v)
 	end
 
 end
 
-function PANEL:AddComboOption(y, id, tbl)
+function PANEL:AddComboOption(x, y, id, tbl)
 
 	self.cb = self:Add("DComboBox")
-	self.cb:SetPos(5, y)
+	self.cb:SetPos(x, y)
 	self.cb:SetTextColor(colWHITE)
 	self.cb:SetText(tbl.info)
 	self.cb:SetSize(100, 20)
@@ -76,28 +80,55 @@ function PANEL:AddComboOption(y, id, tbl)
 	self.cb.lbl:SetTextColor(colWHITE)
 	self.cb.lbl:SetText(tbl.info)
 
-	for k, v in pairs(GAMEMODE:GetDifficulties()) do
-		local choice = GAMEMODE:GetDifficultyText(v)
-		self.cb:AddChoice(choice, v, GAMEMODE:GetDifficulty() == v)
+	for k, v in pairs(tbl.choices) do
+		local text = tbl.choices_text(v)
+		self.cb:AddChoice(text, v, tbl.GetDifficulty() == v)
 	end
 
 	function self.cb:OnSelect(idx, val, data)
-		GAMEMODE:ChangeAdminConfiguration(id,tostring(data))
+		GAMEMODE:ChangeAdminConfiguration(id,data)
 	end
 
 end
 
-function PANEL:AddCheckOption(y, id, tbl)
+function PANEL:AddCheckOption(x, y, id, tbl)
 
 	self.checkb = self:Add("DCheckBoxLabel")
-	self.checkb:SetPos(5, y)
+	self.checkb:SetPos(x, y)
 	self.checkb:SetText(tbl.info)
 	self.checkb:SizeToContents()
-	self.checkb:SetConVar("lambda_" .. id)
+	self.checkb.extra = false
+
+	if not tbl.extra then
+		self.checkb:SetConVar("lambda_" .. id)
+	else
+		self.checkb:SetText(tbl.extra.info)
+		self.checkb:SetValue(tbl.extra.value)
+		self.checkb.extra = true
+		tbl.extra.cached = tbl.value
+	end
+
+	local function Toggled(b)
+		if b then
+			self.Settings[id].numw:SetEnabled(false)
+			self.Settings[id].numw:SetEditable(false)
+		else
+			self.Settings[id].numw:SetEnabled(true)
+			self.Settings[id].numw:SetEditable(true)
+		 end
+	end
 
 	function self.checkb:OnChange(val)
-			if val then val = "1" else val = "0" end
+		if val then val = "1" else val = "0" end
+			if tbl.extra and val == "1" then
+				Toggled(true)
+				GAMEMODE:ChangeAdminConfiguration(id, tostring(0))
+			elseif tbl.extra and val == "0" then
+				Toggled(false)
+				GAMEMODE:ChangeAdminConfiguration(id, tbl.extra.cached)
+			else
 			GAMEMODE:ChangeAdminConfiguration(id, val)
+			end
 	end
 
 end
