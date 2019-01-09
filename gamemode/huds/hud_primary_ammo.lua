0059229b4e15ef6e5319c091cb7aeea03926deb1
Matt <m.moninger.h@gmail.com>
2019-01-09 17:38:42 +0100

    Show SWEP's ammo via its own function if present.

--- gamemode/huds/hud_primary_ammo.lua
+++ gamemode/huds/hud_primary_ammo.lua
@@ -4,108 +4,116 @@ local PANEL = {}
 
 function PANEL:Init()
 
-	self:SetShouldDisplaySecondaryValue(true)
-	self:SetSize(util.ScreenScaleH(103), util.ScreenScaleH(37))
+    self:SetShouldDisplaySecondaryValue(true)
+    self:SetSize(util.ScreenScaleH(103), util.ScreenScaleH(37))
 
-	self:SetLabelText(Localize("#Valve_Hud_AMMO") )
-	self.LastClip1 = 0
-	self.LastClip2 = 0
-	self.LerpValues = false
+    self:SetLabelText(Localize("#Valve_Hud_AMMO") )
+    self.LastClip1 = 0
+    self.LastClip2 = 0
+    self.LerpValues = false
 
-	self.AnimateValueChanged = Derma_Anim("AmmoChanged", self, self.AnimValueChanged)
+    self.AnimateValueChanged = Derma_Anim("AmmoChanged", self, self.AnimValueChanged)
 
-	self.Animations =
-	{
-		self.AnimateValueChanged,
-	}
+    self.Animations =
+    {
+        self.AnimateValueChanged,
+    }
 
 end
 
 function PANEL:Reset()
 
-	self.LastClip1 = -1
-	self.LastClip2 = 0
+    self.LastClip1 = -1
+    self.LastClip2 = 0
 
 end
 
 function PANEL:ShowAmmoCount(state)
-	self:SetShouldDisplaySecondaryValue(state)
-	if state == false then
-		self:SetSize(util.ScreenScaleH(103), util.ScreenScaleH(37))
-	else
-		self:SetSize(util.ScreenScaleH(130), util.ScreenScaleH(37))
-	end
+    self:SetShouldDisplaySecondaryValue(state)
+    if state == false then
+        self:SetSize(util.ScreenScaleH(103), util.ScreenScaleH(37))
+    else
+        self:SetSize(util.ScreenScaleH(130), util.ScreenScaleH(37))
+    end
 end
 
 function PANEL:AnimValueChanged(anim, delta, data)
-	self.Blur = (1 - delta) * 3
-	--self:SetBackgroundColor(0, 0, 0, 128)
-	--self:SetTextColor(255, 208, 64, 255)
+    self.Blur = (1 - delta) * 3
+    --self:SetBackgroundColor(0, 0, 0, 128)
+    --self:SetTextColor(255, 208, 64, 255)
 end
 
 function PANEL:StopAnimations()
-	for _,v in pairs(self.Animations) do
-		v:Stop()
-	end
+    for _,v in pairs(self.Animations) do
+        v:Stop()
+    end
 end
 
 function PANEL:Think()
 
-	local ply = LocalPlayer()
-	if not IsValid(ply) then
-		return
-	end
+    local ply = LocalPlayer()
+    if not IsValid(ply) then
+        return
+    end
 
-	for _,v in pairs(self.Animations) do
-		if v:Active() then
-			v:Run()
-		end
-	end
+    for _,v in pairs(self.Animations) do
+        if v:Active() then
+            v:Run()
+        end
+    end
 
-	local vehicle = ply:GetVehicle()
-	local clip1
-	local ammo = 0
-	local ammotype = -1
+    local vehicle = ply:GetVehicle()
+    local clip1
+    local ammo = 0
+    local ammotype = -1
 
-	if vehicle ~= nil and IsValid(vehicle) and vehicle:GetNWBool("IsPassengerSeat", false) == false then
+    if vehicle ~= nil and IsValid(vehicle) and vehicle:GetNWBool("IsPassengerSeat", false) == false then
 
-		if vehicle.GetAmmo ~= nil then
-			ammotype, clip1, num = vehicle:GetAmmo()
-		end
+        if vehicle.GetAmmo ~= nil then
+            ammotype, clip1, num = vehicle:GetAmmo()
+        end
 
-		if clip1 ~= self.LastClip1 then
-			self.AnimateValueChanged:Start(2)
-			self.LastClip1 = clip1
-		end
+        if clip1 ~= self.LastClip1 then
+            self.AnimateValueChanged:Start(2)
+            self.LastClip1 = clip1
+        end
 
-		self:SetDisplayValue(num)
-		self:SetShouldDisplaySecondaryValue(false)
-		self:SetSecondaryValue(0)
+        self:SetDisplayValue(num)
+        self:SetShouldDisplaySecondaryValue(false)
+        self:SetSecondaryValue(0)
 
-	else
+    else
 
-		local wep = ply:GetActiveWeapon()
-		if wep ~= nil and not IsValid(wep) then
-			return
-		end
+        local wep = ply:GetActiveWeapon()
+        if wep ~= nil and not IsValid(wep) then
+            return
+        end
 
-		ammotype = wep:GetPrimaryAmmoType()
-		ammo = ply:GetAmmoCount(ammotype)
+        if wep.Ammo1 ~= nil and isfunction(wep.Ammo1) then
+            ammo = wep:Ammo1()
+        else
+            ammotype = wep:GetPrimaryAmmoType()
+            ammo = ply:GetAmmoCount(ammotype)
+        end
 
-		clip1 = wep:Clip1()
-		if clip1 == -1 then
-			clip1 = ply:GetAmmoCount(ammotype)
-		end
+        clip1 = wep:Clip1()
+        if clip1 == -1 then
+            if wep.Ammo1 ~= nil and isfunction(wep.Ammo1) then
+                clip1 = wep:Ammo1()
+            else
+                clip1 = ply:GetAmmoCount(ammotype)
+            end
+        end
 
-		if clip1 ~= self.LastClip1 then
-			self.AnimateValueChanged:Start(2)
-			self.LastClip1 = clip1
-		end
+        if clip1 ~= self.LastClip1 then
+            self.AnimateValueChanged:Start(2)
+            self.LastClip1 = clip1
+        end
 
-		self:SetDisplayValue(clip1)
-		self:SetSecondaryValue(ammo)
+        self:SetDisplayValue(clip1)
+        self:SetSecondaryValue(ammo)
 
-	end
+    end
 
 end
 
--- gamemode/huds/hud_secondary_ammo.lua
+++ gamemode/huds/hud_secondary_ammo.lua
@@ -4,68 +4,73 @@ local PANEL = {}
 
 function PANEL:Init()
 
-	self:SetShouldDisplaySecondaryValue(false)
-	self:SetSize(util.ScreenScaleH(80), util.ScreenScaleH(37))
+    self:SetShouldDisplaySecondaryValue(false)
+    self:SetSize(util.ScreenScaleH(80), util.ScreenScaleH(37))
 
-	self:SetLabelText(Localize("#Valve_Hud_AMMO_ALT") )
-	self.LastClip1 = 0
-	self.LastClip2 = 0
-	self.LerpValues = false
+    self:SetLabelText(Localize("#Valve_Hud_AMMO_ALT") )
+    self.LastClip1 = 0
+    self.LastClip2 = 0
+    self.LerpValues = false
 
-	self.AnimateValueChanged = Derma_Anim("AmmoChanged", self, self.AnimValueChanged)
+    self.AnimateValueChanged = Derma_Anim("AmmoChanged", self, self.AnimValueChanged)
 
-	self.Animations =
-	{
-		self.AnimateValueChanged,
-	}
+    self.Animations =
+    {
+        self.AnimateValueChanged,
+    }
 
 end
 
 function PANEL:Reset()
 
-	self.LastClip1 = 0
-	self.LastClip2 = 0
+    self.LastClip1 = 0
+    self.LastClip2 = 0
 
 end
 
 function PANEL:AnimValueChanged(anim, delta, data)
-	self.Blur = (1 - delta) * 3
-	--self:SetBackgroundColor(0, 0, 0, 128)
-	--self:SetTextColor(255, 208, 64, 255)
+    self.Blur = (1 - delta) * 3
+    --self:SetBackgroundColor(0, 0, 0, 128)
+    --self:SetTextColor(255, 208, 64, 255)
 end
 
 function PANEL:StopAnimations()
-	for _,v in pairs(self.Animations) do
-		v:Stop()
-	end
+    for _,v in pairs(self.Animations) do
+        v:Stop()
+    end
 end
 
 function PANEL:Think()
 
-	local ply = LocalPlayer()
-	if not IsValid(ply) then
-		return
-	end
+    local ply = LocalPlayer()
+    if not IsValid(ply) then
+        return
+    end
 
-	local wep = ply:GetActiveWeapon()
-	if not IsValid(wep) then
-		return
-	end
+    local wep = ply:GetActiveWeapon()
+    if not IsValid(wep) then
+        return
+    end
 
-	for _,v in pairs(self.Animations) do
-		if v:Active() then
-			v:Run()
-		end
-	end
+    for _,v in pairs(self.Animations) do
+        if v:Active() then
+            v:Run()
+        end
+    end
 
-	local altAmmo = ply:GetAmmoCount(wep:GetSecondaryAmmoType())
+    local altAmmo = 0
+    if wep.Ammo2 ~= nil and isfunction(wep.Ammo2) then
+        altAmmo = wep:Ammo2()
+    else
+        altAmmo = ply:GetAmmoCount(wep:GetSecondaryAmmoType())
+    end
 
-	if altAmmo ~= self.LastClip1 then
-		self.AnimateValueChanged:Start(2)
-		self.LastClip1 = altAmmo
-	end
+    if altAmmo ~= self.LastClip1 then
+        self.AnimateValueChanged:Start(2)
+        self.LastClip1 = altAmmo
+    end
 
-	self:SetDisplayValue(altAmmo)
+    self:SetDisplayValue(altAmmo)
 
 end
 
