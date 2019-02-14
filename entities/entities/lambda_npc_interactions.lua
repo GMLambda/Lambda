4f86a1088d568dfc22a8d91205c9e82a4b9d76ec
Matt <m.moninger.h@gmail.com>
2019-02-14 12:53:50 +0100

    Fix motion controller not setting BlocksLOS

---
+++ entities/entities/lambda_npc_interactions.lua
@@ -0,0 +1,61 @@
+ENT.Base = "lambda_entity"
+ENT.Type = "point"
+
+DEFINE_BASECLASS("lambda_entity")
+
+function ENT:PreInitialize()
+    BaseClass.PreInitialize(self)
+    DbgPrint(self, "PreInitialize")
+end
+
+function ENT:Initialize()
+    BaseClass.Initialize(self)
+    DbgPrint(self, "Initialize")
+end
+
+local COND_SEE_PLAYER = 32
+
+function ENT:SearchForInteractTargets()
+    local owner = self:GetOwner()
+    if not IsValid(owner) then
+        return
+    end
+
+    local visible = {}
+    for _,v in pairs(player.GetAll()) do
+        if v:Alive() == false then
+            continue
+        end
+        if owner:Visible(v) then
+            table.insert(visible, v)
+        end
+    end
+
+    if #visible == 0 then
+        return
+    else
+        owner:SetCondition(COND_SEE_PLAYER)
+    end
+
+    for _,v in pairs(visible) do
+        local wep = v:GetActiveWeapon()
+        if IsValid(wep) == false or wep:GetClass() ~= "weapon_physcannon" then
+            continue
+        end
+        local heldObject = wep:GetAttachedObject()
+        if IsValid(heldObject) then
+            print("Set hack target")
+            owner:SetSaveValue("m_hHackTarget", heldObject)
+        end
+    end
+end
+
+function ENT:Think()
+    self:NextThink(CurTime() + 1)
+    self:SearchForInteractTargets()
+    return true
+end
+
+function ENT:UpdateTransmitState()
+    return TRANSMIT_NEVER
+end
