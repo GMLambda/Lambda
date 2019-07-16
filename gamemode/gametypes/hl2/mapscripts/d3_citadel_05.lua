1ac9898f22eda1d74f094c696a346f4719085e89
Matt <m.moninger.h@gmail.com>
2019-07-16 16:13:19 +0200

    Fix default weapons not being removed on d3_citadel_05

--- gamemode/gametypes/hl2/mapscripts/d3_citadel_05.lua
+++ gamemode/gametypes/hl2/mapscripts/d3_citadel_05.lua
@@ -31,6 +31,9 @@ MAPSCRIPT.EntityFilterByClass =
 
 MAPSCRIPT.EntityFilterByName =
 {
+    ["global_newgame_template_ammo"] = true,
+    ["global_newgame_template_local_items"] = true,
+    ["global_newgame_template_base_items"] = true,
 }
 
 MAPSCRIPT.GlobalStates =
