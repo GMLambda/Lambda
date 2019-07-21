81065dc27c57dd90a2c21704476db1551e5854a9
Matt <m.moninger.h@gmail.com>
2019-07-21 16:53:36 +0200

    Replace env_credits and hud_credits with lua version

--- gamemode/huds/hud_credits.lua
+++ gamemode/huds/hud_credits.lua
@@ -15,14 +15,14 @@ local CREDITS_SECTION_NAME =
 }
 
 local function CreateFonts()
-	surface.CreateFont("LambdaCreditsOutroText", {
+	surface.CreateFont("LambdaCreditsOutroTextRemap", {
 		font = "Verdana",
 		size = util.ScreenScaleH(9),
 		weight = 900,
 		antialias = true,
 		additive = false
 	})
-	surface.CreateFont("LambdaCreditsOutroLogos", {
+	surface.CreateFont("LambdaCreditsOutroLogosRemap", {
 		font = "HalfLife2",
 		size = util.ScreenScaleH(48),
 		weight = 0,
@@ -30,11 +30,29 @@ local function CreateFonts()
 		additive = true,
 		custom = true,
 	})
+	surface.CreateFont("LambdaCreditsTextRemap", {
+		font = "Verdana",
+		size = util.ScreenScaleH(12),
+		weight = 900,
+		antialias = true,
+		additive = true,
+	})
+	surface.CreateFont("LambdaWeaponIconsRemap", {
+		font = "HalfLife2",
+		size = util.ScreenScaleH(64),
+		weight = 0,
+		antialias = true,
+		additive = true,
+		custom = true,
+	})
 end
 
-local FONT_REMAP = {
-	["CreditsOutroText"] = "LambdaCreditsOutroText",
-	["CreditsOutroLogos"] = "LambdaCreditsOutroLogos",
+local FONT_REMAP =
+{
+	["CreditsOutroText"] = "LambdaCreditsOutroTextRemap",
+	["CreditsOutroLogos"] = "LambdaCreditsOutroLogosRemap",
+	["CreditsText"] = "LambdaCreditsTextRemap",
+	["WeaponIcons"] = "LambdaWeaponIconsRemap",
 }
 
 function PANEL:Init()
@@ -69,15 +87,37 @@ function PANEL:ShowCredits(credits, params, creditsType, startTime, finishTime)
 		error("Invalid credits section supplied: " .. name)
 	end
 
-	print("Set sections")
-
 	self.CreditsTime = finishTime - startTime
 	if creditsType == CREDITS_TYPE_OUTRO then
 		self:ComputeSize()
 		self.ScrollEndTime = finishTime
 		self.ScrollY = 0
 	elseif creditsType == CREDITS_TYPE_INTRO then
+		local i = 0
+		local creditsSize = #self.Section
+		local ts = startTime
 
+		local fadeInTime = self.Params["fadeintime"] or 1.0
+		local fadeOutTime = self.Params["fadeouttime"] or 1.0
+		local fadeHoldTime = self.Params["fadeholdtime"] or 1.0
+		local nextFadeTime = self.Params["nextfadetime"] or 1.0
+		local pauseBetweenWaves = self.Params["pausebetweenwaves"] or 1.0
+		local entryLength = fadeInTime + fadeOutTime + fadeHoldTime
+
+		while i < creditsSize do
+			local len = 3
+			if i + len > creditsSize then
+				len = creditsSize - i
+			end
+			for n = 1, len do
+				local entry = self.Section[i + n]
+				entry.startTime = ts
+				entry.finishTime = ts + entryLength
+				ts = ts + nextFadeTime
+			end
+			ts = ts + pauseBetweenWaves
+			i = i + len
+		end
 	end
 
 	self.CreditsType = creditsType
@@ -102,9 +142,57 @@ function PANEL:ComputeSize()
 
 end
 
+function PANEL:DrawCreditsEntry(entry, curTime, posX, posY)
+	local elapsed = curTime - entry.startTime
+	local fadeInTime = self.Params["fadeintime"] or 1.0
+	local fadeOutTime = self.Params["fadeouttime"] or 1.0
+	local fadeHoldTime = self.Params["fadeholdtime"] or 1.0
+
+	local alpha = 0
+	if elapsed < fadeInTime then
+		alpha = elapsed / fadeInTime
+	elseif elapsed > fadeInTime and elapsed < (fadeInTime + fadeHoldTime) then
+		alpha = 1
+	elseif elapsed >= fadeInTime + fadeHoldTime then
+		local fadeElapsed = elapsed - fadeInTime - fadeHoldTime
+		alpha = 1.0 - (fadeElapsed / fadeOutTime)
+	end
+
+	local font = FONT_REMAP[entry.Value] or entry.Value
+	surface.SetFont(font)
+	surface.SetTextPos(util.ScreenScaleH(posX), util.ScreenScaleH(posY))
+	local color = self.Params["color"]
+	if color ~= nil then
+		surface.SetTextColor( color.r, color.g, color.b, color.a * alpha )
+	else
+		surface.SetTextColor( 255, 255, 255 )
+	end
+	surface.DrawText(entry.Key)
+end
+
 function PANEL:PaintIntroCredits(width, height)
 
-	local totalTime
+	local curTime = CurTime()
+	local posX = self.Params["posx"]
+	local posY = self.Params["posy"]
+
+	local i = 0
+	local creditsSize = #self.Section
+	while i < creditsSize do
+		local len = 3
+		if i + len > creditsSize then
+			len = creditsSize - i
+		end
+
+		for n = 1, len do
+			local entry = self.Section[i + n]
+			if curTime < entry.startTime or curTime > entry.finishTime then
+				continue
+			end
+			self:DrawCreditsEntry(entry, curTime, posX + 100, posY + ((n - 1) * 20))
+		end
+		i = i + len
+	end
 
 end
 
