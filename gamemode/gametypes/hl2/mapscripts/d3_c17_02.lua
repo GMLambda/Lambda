919600af9f62d958f2ecb7cb36cc8937c9722953
Matt <m.moninger.h@gmail.com>
2019-09-09 06:43:13 +0200

    Fix d3_c17_02 having default player loadout

--- gamemode/gametypes/hl2/mapscripts/d3_c17_02.lua
+++ gamemode/gametypes/hl2/mapscripts/d3_c17_02.lua
@@ -43,12 +43,13 @@ MAPSCRIPT.InputFilters =
 
 MAPSCRIPT.EntityFilterByClass =
 {
-    --["env_global"] = true,
 }
 
 MAPSCRIPT.EntityFilterByName =
 {
-    --["pclip_gate1"] = true,
+    ["global_newgame_template_base_items"] = true,
+    ["global_newgame_template_local_items"] = true,
+    ["global_newgame_template_ammo"] = true,
 }
 
 function MAPSCRIPT:Init()
