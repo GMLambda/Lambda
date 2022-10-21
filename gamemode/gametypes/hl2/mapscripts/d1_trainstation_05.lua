93fd89a0e7b77d275d32cbcc757cf0e93e80551a
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-10-21 06:02:29 +0300

    Improve d1_trainstation_05 map script

--- gamemode/gametypes/hl2/mapscripts/d1_trainstation_05.lua
+++ gamemode/gametypes/hl2/mapscripts/d1_trainstation_05.lua
@@ -14,8 +14,6 @@ MAPSCRIPT.DefaultLoadout =
 
 MAPSCRIPT.InputFilters =
 {
-    ["lab_door"] = { "Close" },
-    ["lab_door_clip"] = { "Close" },
 }
 
 MAPSCRIPT.EntityFilterByClass =
@@ -43,6 +41,13 @@ function MAPSCRIPT:PostInit()
 
         ents.WaitForEntityByName("player_in_teleport", function(ent)
             ent:SetKeyValue("teamwait", "1")
+            ent.OnTrigger = function(e)
+                ents.WaitForEntityByName("kleiner_teleport_lift_1", function(ent)
+                    local cpLift = GAMEMODE:CreateCheckpoint(Vector(-7185.238770, -1185.304810, 6.031250), Angle(0, -90, 0))
+                    cpLift:SetParent(ent)
+                    GAMEMODE:SetPlayerCheckpoint(cpLift)
+                end)
+            end
         end)
 
         ents.WaitForEntityByName("start_first_teleport_01", function(ent)
@@ -54,18 +59,11 @@ function MAPSCRIPT:PostInit()
             --ent:SetKeyValue("dmg", "0") -- Don`t hurt the player
         end)
 
-        local mapscript = self
-
         -- Enable HEV as soon someone gets it.
         GAMEMODE:WaitForInput("suiton", "Enable", function(ent)
             local loadout = GAMEMODE:GetMapScript().DefaultLoadout
             loadout.HEV = true
         end)
-
-        for _,v in pairs(ents.FindByClass("item_suit")) do
-            -- Typically not an item we respawn at all times, manually flag it.
-            v:EnableRespawn(true)
-        end
 
         local allowPlayerClip = false
         GAMEMODE:WaitForInput("brush_soda_clip_player_2", "Enable", function(ent)
@@ -112,6 +110,77 @@ function MAPSCRIPT:PostInit()
         ents.WaitForEntityByName("gman_fixtie_1", function(ent)
             ent:SetKeyValue("m_iszPlay", "citizen4_valve")
         end)
+
+        -- Create a trigger that requires all players to have the suit
+        local suitTrigger = ents.Create("trigger_once")
+        suitTrigger:SetName("lambda_suit_trigger")
+        suitTrigger:SetKeyValue("StartDisabled", "1")
+        suitTrigger:SetupTrigger(
+            Vector(-6716.156250, -1396.862793, 0), 
+            Angle(0,0,0),
+            Vector(-350, -300, 0),
+            Vector(350, 310, 250),
+            true,
+            SF_TRIGGER_ONLY_PLAYER_WITH_HEV
+        )
+        suitTrigger:SetKeyValue("teamwait", "1")
+        suitTrigger:SetKeyValue("messagepos", "231 -221 154")
+        suitTrigger:SetKeyValue("timeoutteleport", "0")
+        suitTrigger:Fire("AddOutput", "OnTrigger suitnag_loopall01_lcs,Kill,,0,-1")
+        suitTrigger:Fire("AddOutput", "OnTrigger hev_light_suit_1,TurnOff,,0,-1")
+        suitTrigger:Fire("AddOutput", "OnTrigger get_suit_math_1,Add,1,0,-1")
+        suitTrigger:Fire("AddOutput", "OnTrigger hevnag_speaker,Kill,,0,-1")
+        suitTrigger:Fire("AddOutput", "OnTrigger phys_knocked_nag_rl,Kill,,0,-1")
+        suitTrigger.OnWaitTimeout = function(s)
+            for _,v in pairs(player.GetAll()) do
+                v:EquipSuit()
+            end
+        end
+
+        -- Make the song suit per client.
+        ents.WaitForEntityByName("song_suit", function(ent)
+            local song_suit = ents.Create("lambda_client_sound")
+            song_suit:SetName("song_suit")
+            song_suit:SetKeyValue("sound", "song_trainstation_05_suit")
+            song_suit:Spawn()
+
+            -- Remove old.
+            ent:Remove()
+        end)
+
+        -- Remove the original suit and replace with custom
+        local suits = ents.FindByClass("item_suit")
+        local itemSuit = suits[1]
+        if IsValid(itemSuit) then
+            local newSuit = ents.Create("item_suit")
+            newSuit:SetPos(itemSuit:GetPos())
+            newSuit:SetAngles(itemSuit:GetAngles())
+            newSuit:Spawn()
+            newSuit:Fire("AddOutput", "OnPlayerTouch song_suit,PlaySound,,0,-1")
+            newSuit:Fire("AddOutput", "OnPlayerTouch lambda_suit_trigger,Enable,,0,-1")
+            newSuit:EnableRespawn(true)
+
+            -- Remove old.
+            itemSuit:Remove()
+        end
+
+        GAMEMODE:WaitForInput("lab_door", "Close", function()
+            local cp = GAMEMODE:CreateCheckpoint(Vector(-7154, -1508.3, 1), Angle(0, 90, 0))
+            GAMEMODE:SetPlayerCheckpoint(cp, nil)
+        end)
+
+        -- Outdoor
+        local checkpoint2 = GAMEMODE:CreateCheckpoint(Vector(-10368, -4714, 320), Angle(0, 180, 0))
+        local checkpointTrigger2 = ents.Create("trigger_once")
+        checkpointTrigger2:SetupTrigger(
+            Vector(-10368, -4714, 320),
+            Angle(0, 0, 0),
+            Vector(-50, -50, 0),
+            Vector(50, 50, 180)
+        )
+        checkpointTrigger2.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(checkpoint2, activator)
+        end        
 
     end
 
