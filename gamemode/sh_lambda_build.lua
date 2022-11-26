52b64f6aec6c4f417b2e5b2cffe995ec8ecef644
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-11-26 04:51:45 +0200

    Improve workflows

--- gamemode/sh_lambda_build.lua
+++ gamemode/sh_lambda_build.lua
@@ -6,11 +6,10 @@ GM.Name = "Lambda"
 GM.Author = "N/A"
 GM.Email = "N/A"
 GM.Website = "https://github.com/ZehMatt/Lambda"
-GM.Version = "0.9.14"
 
-if LAMBDA_STAGING then
-    GM.WorkshopID = "801875828"
-else
-    GM.WorkshopID = "780244493"
-end
+-- Testing: 801875828
+-- Release: 780244493
+-- Values will be overwritten by the CI for automated deployment.
+GM.WorkshopID = "801875828"
 GM.WorkshopBuild = false
+GM.Version = "develop"
