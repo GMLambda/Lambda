6fe6870ee11ac518b9fe8db9997c176a6bfa7112
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-07-22 16:14:17 +0300

    Refactor d3_c17_03 checkpoints

--- gamemode/gametypes/hl2/mapscripts/d3_c17_03.lua
+++ gamemode/gametypes/hl2/mapscripts/d3_c17_03.lua
@@ -4,7 +4,6 @@ end
 
 local MAPSCRIPT = {}
 MAPSCRIPT.PlayersLocked = false
-
 MAPSCRIPT.DefaultLoadout = {
     Weapons = {"weapon_lambda_medkit", "weapon_crowbar", "weapon_pistol", "weapon_smg1", "weapon_357", "weapon_physcannon", "weapon_frag", "weapon_shotgun", "weapon_ar2", "weapon_rpg", "weapon_crossbow", "weapon_bugbait"},
     Ammo = {
@@ -24,7 +23,6 @@ MAPSCRIPT.DefaultLoadout = {
 
 MAPSCRIPT.InputFilters = {}
 MAPSCRIPT.EntityFilterByClass = {} --["env_global"] = true,
-
 MAPSCRIPT.EntityFilterByName = {
     ["pclip_gate1"] = true,
     ["global_newgame_template_base_items"] = true,
@@ -32,26 +30,31 @@ MAPSCRIPT.EntityFilterByName = {
     ["global_newgame_template_ammo"] = true
 }
 
+MAPSCRIPT.Checkpoints = {
+    {
+        Pos = Vector(-2706.719727, -2843.710449, -3.968750),
+        Ang = Angle(0, 90, 0),
+        Trigger = {
+            Pos = Vector(-2706.719727, -2843.710449, -3.968750),
+            Mins = Vector(-350, -300, 0),
+            Maxs = Vector(350, 300, 100),
+        }
+    },
+}
+
 function MAPSCRIPT:PostInit()
     if SERVER then
-        ents.WaitForEntityByName("trigger_tower", function(ent)
-            -- Fail safe in case one decides to kill his allies.
-            ent:Fire("AddOutput", "OnTrigger plaza_exit_relay,Trigger,,40,-1")
-        end)
-
-        -- setpos -497.127838 29.422707 576.030090;setang 1.708000 -178.566528 0.000000
-        local checkpoint1 = GAMEMODE:CreateCheckpoint(Vector(-2706.719727, -2843.710449, -3.968750), Angle(0, 90, 0))
-        local checkpointTrigger1 = ents.Create("trigger_once")
-        checkpointTrigger1:SetupTrigger(Vector(-2706.719727, -2843.710449, -3.968750), Angle(0, 0, 0), Vector(-350, -300, 0), Vector(350, 300, 100))
-
-        checkpointTrigger1.OnTrigger = function(_, activator)
-            GAMEMODE:SetPlayerCheckpoint(checkpoint1, activator)
-        end
+        ents.WaitForEntityByName(
+            "trigger_tower",
+            function(ent)
+                -- Fail safe in case one decides to kill his allies.
+                ent:Fire("AddOutput", "OnTrigger plaza_exit_relay,Trigger,,40,-1")
+            end
+        )
     end
 end
 
 function MAPSCRIPT:PostPlayerSpawn(ply)
-    --DbgPrint("PostPlayerSpawn")
 end
 
 return MAPSCRIPT
