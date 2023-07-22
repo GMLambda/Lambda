f98d70bce3f9734cf19e3ffb59fb5d6796864b6b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-07-22 16:05:04 +0300

    Refactor d2_prison_02 checkpoints

--- gamemode/gametypes/hl2/mapscripts/d2_prison_02.lua
+++ gamemode/gametypes/hl2/mapscripts/d2_prison_02.lua
@@ -4,7 +4,6 @@ end
 
 local MAPSCRIPT = {}
 MAPSCRIPT.PlayersLocked = false
-
 MAPSCRIPT.DefaultLoadout = {
     Weapons = {"weapon_lambda_medkit", "weapon_crowbar", "weapon_pistol", "weapon_smg1", "weapon_357", "weapon_physcannon", "weapon_frag", "weapon_shotgun", "weapon_ar2", "weapon_rpg", "weapon_crossbow", "weapon_bugbait"},
     Ammo = {
@@ -24,7 +23,6 @@ MAPSCRIPT.DefaultLoadout = {
 
 MAPSCRIPT.InputFilters = {}
 MAPSCRIPT.EntityFilterByClass = {}
-
 MAPSCRIPT.EntityFilterByName = {
     ["global_newgame_template_base_items"] = true,
     ["global_newgame_template_local_items"] = true,
@@ -35,35 +33,39 @@ MAPSCRIPT.GlobalStates = {
     ["antlion_allied"] = GLOBAL_ON
 }
 
+MAPSCRIPT.Checkpoints = {
+    {
+        Pos = Vector(-1384.557129, 2894.679688, 384.031250),
+        Ang = Angle(0, 45, 0),
+        Trigger = {
+            Pos = Vector(-1384.557129, 2894.679688, 384.031250),
+            Mins = Vector(-100, -130, 0),
+            Maxs = Vector(50, 100, 200),
+        }
+    },
+    {
+        Pos = Vector(-1951.139526, 2554.889893, 576.031250),
+        Ang = Angle(0, 45, 0),
+        VisiblePos = Vector(-1950.105469, 2715.927734, 512.031250),
+        Trigger = {
+            Pos = Vector(-1952.149292, 2550.710938, 512.023621),
+            Mins = Vector(-100, -250, 0),
+            Maxs = Vector(100, 250, 200),
+        }
+    },
+}
+
 function MAPSCRIPT:PostInit()
-    ents.WaitForEntityByName("door_2", function(ent)
-        ent:Fire("Unlock")
-    end)
-
-    if SERVER then
-        --setpos -1384.557129 2894.679688 448.031250;setang -1.010203 -63.622845 0.000000
-        local checkpoint1 = GAMEMODE:CreateCheckpoint(Vector(-1384.557129, 2894.679688, 384.031250), Angle(0, 45, 0))
-        local checkpointTrigger1 = ents.Create("trigger_once")
-        checkpointTrigger1:SetupTrigger(Vector(-1384.557129, 2894.679688, 384.031250), Angle(0, 0, 0), Vector(-100, -130, 0), Vector(50, 100, 200))
-
-        checkpointTrigger1.OnTrigger = function(_, activator)
-            GAMEMODE:SetPlayerCheckpoint(checkpoint1, activator)
+    ents.WaitForEntityByName(
+        "door_2",
+        function(ent)
+            ent:Fire("Unlock")
         end
-
-        -- cp 2 setpos -1951.139526 2554.889893 576.031250;setang 6.475924 -93.248947 0.000000
-        local checkpoint2 = GAMEMODE:CreateCheckpoint(Vector(-1951.139526, 2554.889893, 576.031250), Angle(0, 45, 0))
-        checkpoint2:SetVisiblePos(Vector(-1950.105469, 2715.927734, 512.031250))
-        local checkpointTrigger2 = ents.Create("trigger_once")
-        checkpointTrigger2:SetupTrigger(Vector(-1952.149292, 2550.710938, 512.023621), Angle(0, 0, 0), Vector(-100, -250, 0), Vector(100, 250, 200))
-
-        checkpointTrigger2.OnTrigger = function(_, activator)
-            GAMEMODE:SetPlayerCheckpoint(checkpoint2, activator)
-        end
-    end
+    )
 end
 
 function MAPSCRIPT:PostPlayerSpawn(ply)
-    --DbgPrint("PostPlayerSpawn")
 end
 
 return MAPSCRIPT
+--DbgPrint("PostPlayerSpawn")
