9594d62b412461e218398fe9500ee9b8de6b91c7
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-07-22 16:22:43 +0300

    Refactor d3_c17_09 checkpoints

--- gamemode/gametypes/hl2/mapscripts/d3_c17_09.lua
+++ gamemode/gametypes/hl2/mapscripts/d3_c17_09.lua
@@ -4,7 +4,6 @@ end
 
 local MAPSCRIPT = {}
 MAPSCRIPT.PlayersLocked = false
-
 MAPSCRIPT.DefaultLoadout = {
     Weapons = {"weapon_lambda_medkit", "weapon_crowbar", "weapon_pistol", "weapon_smg1", "weapon_357", "weapon_physcannon", "weapon_frag", "weapon_shotgun", "weapon_ar2", "weapon_rpg", "weapon_crossbow", "weapon_bugbait"},
     Ammo = {
@@ -28,56 +27,35 @@ MAPSCRIPT.InputFilters = {
 }
 
 MAPSCRIPT.EntityFilterByClass = {}
-
 MAPSCRIPT.EntityFilterByName = {
     ["player_spawn_items_maker"] = true
 }
 
+MAPSCRIPT.Checkpoints = {
+    {
+        Pos = Vector(7405.353516, 6305.318848, 0.031250),
+        Ang = Angle(0, 180, 0),
+        Trigger = {
+            Pos = Vector(7099.762695, 6237.561523, 0.031250),
+            Mins = Vector(-130, -130, 0),
+            Maxs = Vector(130, 130, 100),
+        }
+    },
+    {
+        Pos = Vector(6000.681641, 6446.313477, 96.031250),
+        Ang = Angle(0, 0, 0),
+        Trigger = {
+            Pos = Vector(6000.681641, 6446.313477, 96.031250),
+            Mins = Vector(-130, -130, 0),
+            Maxs = Vector(130, 130, 100),
+        }
+    },
+}
+
 function MAPSCRIPT:PostInit()
-    if SERVER then
-        -- 7099.762695 6237.561523 0.031250
-        local checkpoint1 = GAMEMODE:CreateCheckpoint(Vector(7405.353516, 6305.318848, 0.031250), Angle(0, 180, 0))
-        local checkpointTrigger1 = ents.Create("trigger_once")
-        checkpointTrigger1:SetupTrigger(Vector(7099.762695, 6237.561523, 0.031250), Angle(0, 0, 0), Vector(-130, -130, 0), Vector(130, 130, 100))
-
-        checkpointTrigger1.OnTrigger = function(_, activator)
-            GAMEMODE:SetPlayerCheckpoint(checkpoint1, activator)
-        end
-
-        -- 6000.681641 6446.313477 96.031250
-        local checkpoint2 = GAMEMODE:CreateCheckpoint(Vector(6000.681641, 6446.313477, 96.031250), Angle(0, 0, 0))
-        local checkpointTrigger2 = ents.Create("trigger_once")
-        checkpointTrigger2:SetupTrigger(Vector(6000.681641, 6446.313477, 96.031250), Angle(0, 0, 0), Vector(-130, -130, 0), Vector(130, 130, 100))
-
-        checkpointTrigger2.OnTrigger = function(_, activator)
-            GAMEMODE:SetPlayerCheckpoint(checkpoint2, activator)
-        end
-        --[[
-        ents.WaitForEntityByName("sniper1", function(ent)
-            ent:SetName("lambda_sniper1")
-        end)
-
-        ents.WaitForEntityByName("sniper2", function(ent)
-            ent:SetName("lambda_sniper2")
-        end)
-
-        ents.WaitForEntityByName("sniper3", function(ent)
-            ent:SetName("lambda_sniper3")
-        end)
-
-        ents.WaitForEntityByName("sniper4", function(ent)
-            ent:SetName("lambda_sniper4")
-        end)
-
-        ents.WaitForEntityByName("sniper5", function(ent)
-            ent:SetName("lambda_sniper5")
-        end)
-        ]]
-    end
 end
 
 function MAPSCRIPT:PostPlayerSpawn(ply)
-    --DbgPrint("PostPlayerSpawn")
 end
 
 return MAPSCRIPT
