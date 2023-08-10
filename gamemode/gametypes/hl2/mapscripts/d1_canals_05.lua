7b9d6b271c9d9df272aa832c41ee4ea08423c693
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-08-10 20:16:13 +0300

    Improve some section of d1_canals_05

--- gamemode/gametypes/hl2/mapscripts/d1_canals_05.lua
+++ gamemode/gametypes/hl2/mapscripts/d1_canals_05.lua
@@ -4,7 +4,6 @@ end
 
 local MAPSCRIPT = {}
 MAPSCRIPT.PlayersLocked = false
-
 MAPSCRIPT.DefaultLoadout = {
     Weapons = {"weapon_lambda_medkit", "weapon_crowbar", "weapon_pistol", "weapon_smg1"},
     Ammo = {
@@ -17,11 +16,20 @@ MAPSCRIPT.DefaultLoadout = {
 
 MAPSCRIPT.InputFilters = {}
 MAPSCRIPT.EntityFilterByClass = {}
-
 MAPSCRIPT.EntityFilterByName = {
     ["global_newgame_entmaker"] = true,
     ["relay_rockfall_start"] = true -- Don't do that, its trivial.
 }
+
+local function FreezeEntitiesAt(pos, extent)
+    local foundEnts = ents.FindInBox(pos - extent, pos + extent)
+    for _, v in pairs(foundEnts) do
+        local physObj = v:GetPhysicsObject()
+        if IsValid(physObj) then
+            physObj:EnableMotion(false)
+        end
+    end
+end
 
 function MAPSCRIPT:PostInit()
     if SERVER then
@@ -29,7 +37,6 @@ function MAPSCRIPT:PostInit()
         checkpoint1:SetVisiblePos(Vector(4240.543945, 3220.031982, -473.430939))
         local checkpointTrigger1 = ents.Create("trigger_once")
         checkpointTrigger1:SetupTrigger(Vector(4236.846191, 3261.946289, -474.814972), Angle(0, 0, 0), Vector(-100, -100, 0), Vector(100, 100, 180))
-
         checkpointTrigger1.OnTrigger = function(_, activator)
             GAMEMODE:SetPlayerCheckpoint(checkpoint1, activator)
         end
@@ -38,21 +45,24 @@ function MAPSCRIPT:PostInit()
         checkpoint2:SetVisiblePos(Vector(6758.199219, 1572.104248, -447.968750))
         local checkpointTrigger2 = ents.Create("trigger_once")
         checkpointTrigger2:SetupTrigger(Vector(6770.862793, 1569.191040, -447.968750), Angle(0, 0, 0), Vector(-100, -100, 0), Vector(100, 100, 180))
-
         checkpointTrigger2.OnTrigger = function(_, activator)
             GAMEMODE:SetPlayerCheckpoint(checkpoint2, activator)
         end
 
-        ents.WaitForEntityByName("rotate_guncave_exit_wheel", function(ent)
-            ent:Fire("Unlock")
-            ent:Fire("AddOutput", "OnPressed relay_airboat_gateopen,Trigger")
-            ent:Fire("AddOutput", "OnPressed ss_arlene_opengate,Kill") -- In case the player opens it dont play the scene.
-        end)
-    end
-end
+        ents.WaitForEntityByName(
+            "rotate_guncave_exit_wheel",
+            function(ent)
+                ent:Fire("Unlock")
+                ent:Fire("AddOutput", "OnPressed relay_airboat_gateopen,Trigger")
+                ent:Fire("AddOutput", "OnPressed ss_arlene_opengate,Kill") -- In case the player opens it dont play the scene.
+            end
+        )
 
-function MAPSCRIPT:PostPlayerSpawn(ply)
-    --DbgPrint("PostPlayerSpawn")
+        -- Disable motion of some entities, they are odd to walk on in multiplayer.
+        FreezeEntitiesAt(Vector(4172.168457, 5346.647461, -124.435822), Vector(10, 10, 10))
+        FreezeEntitiesAt(Vector(4397.086914, 5241.035645, -121.793793), Vector(10, 10, 10))
+        FreezeEntitiesAt(Vector(4246.002441, 4931.557129, -121.530907), Vector(10, 10, 10))
+    end
 end
 
 return MAPSCRIPT
