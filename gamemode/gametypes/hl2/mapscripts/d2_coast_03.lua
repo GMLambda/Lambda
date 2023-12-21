729ac9c0be6247f4711721a4005949e6a0271511
dounai2333 <1328600509@qq.com>
2023-12-22 01:14:06 +0800

    Update d2_coast_03 mapscript
    - Fixes a bug exists in the base HL2
    - Give more supplies in the gunship fight scene
    - Some critical NPCs are protected to not die by vehicle crush
    - Block a exploit where player can skip the gunship fight scene

--- gamemode/gametypes/hl2/mapscripts/d2_coast_03.lua
+++ gamemode/gametypes/hl2/mapscripts/d2_coast_03.lua
@@ -42,18 +42,124 @@ function MAPSCRIPT:PostInit()
         -- Combine and their binoculars... it just fucks your game up, nothing else.
         ents.WaitForEntityByName("telescope", function(ent)
             ent:SetKeyValue("wait", "20")
-            ent:SetKeyValue("spawnflags", "4")
+            ent:SetKeyValue("spawnflags", "20")
         end)
 
-        ents.WaitForEntityByName("citizen_b_regular_original", function(ent)
-            ent:SetHealth(100)
+        -- Bug fixes, exist in base HL2: The cleanup relay got triggered before first time use binocular.
+        -- This bug can result player seeing G-Man face to face and duplicated citizens.
+        -- Remove the cutscene related entities about using binocular. Player no longer see cutscene but better safe than sorry.
+        ents.WaitForEntityByName("gman_template", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("citizen_spawn_relay", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("binoccit_1", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("binoccit_2", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("antlion_ragdoll", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("lr_binoc_scene", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("gman_walk", function(ent)
+            ent:Remove()
+        end)
+
+        ents.WaitForEntityByName("odessa_walk", function(ent)
+            ent:Remove()
+        end)
+
+        -- Spawn more supplies
+        local crate_rpg1 = ents.Create("item_ammo_crate")
+        crate_rpg1:SetKeyValue("AmmoType", "3")
+        crate_rpg1:SetPos(Vector(6550, 4186, 276))
+        crate_rpg1:SetAngles(Angle(0, 90, 0))
+        crate_rpg1:Spawn()
+
+        local crate_rpg2 = ents.Create("item_ammo_crate")
+        crate_rpg2:SetKeyValue("AmmoType", "3")
+        crate_rpg2:SetPos(Vector(8998, 4490, 272))
+        crate_rpg2:SetAngles(Angle(0, 180, 0))
+        crate_rpg2:Spawn()
+
+        local medkits =
+        {
+            {7025, 3475, 340},
+            {7000, 3485, 350},
+            {6530, 4355, 280},
+            {7920, 3520, 325},
+            {7895, 3520, 325},
+            {6735, 4790, 330},
+            {6720, 4810, 330},
+            {8365, 4350, 440},
+            {8330, 4330, 440},
+            {8295, 4325, 440},
+            {8870, 4450, 535},
+            {8865, 4425, 535}
+        }
+
+        for _, v in ipairs(medkits) do
+            local medkit = ents.Create("item_healthkit")
+            medkit:SetPos(Vector(v[1], v[2], v[3]))
+            medkit:Spawn()
+        end
+
+        local batteries =
+        {
+            {8685, 4185, 305},
+            {8665, 4175, 305},
+            {8810, 4205, 405},
+            {8415, 4340, 545},
+            {8410, 4345, 545},
+            {6480, 4370, 285}
+        }
+
+        for _, v in ipairs(batteries) do
+            local battery = ents.Create("item_battery")
+            battery:SetPos(Vector(v[1], v[2], v[3]))
+            battery:Spawn()
+        end
+
+        -- Block the road to prevent player skipping the whole scene.
+        -- These are 'func_vehicleclip' and it only do collision with vehicle, force it to collide with player.
+        for _, v in ipairs(ents.FindByModel("*145")) do
+            v:SetCollisionGroup(COLLISION_GROUP_PLAYER)
+            v:SetName("road_exploit_block")
+        end
+
+        for _, v in ipairs(ents.FindByModel("*146")) do
+            v:SetCollisionGroup(COLLISION_GROUP_PLAYER)
+            v:SetName("road_exploit_block")
+        end
+
+        for _, v in ipairs(ents.FindByModel("*154")) do
+            v:SetCollisionGroup(COLLISION_GROUP_PLAYER)
+            v:SetName("road_exploit_block")
+        end
+
+        -- Only the player can pass this filter.
+        -- Prevent vehicle crushing the critical NPCs in accident!
+        local filter_player = ents.Create("filter_activator_class")
+        filter_player:SetName("filter_player")
+        filter_player:SetKeyValue("filterclass", "player")
+
+        ents.WaitForEntityByName("greeter_maker", function(ent)
+            ent:Fire("AddOutput", "OnEntitySpawned citizen_b_regular_original,AddOutput,health 100,0.1,-1")
+            ent:Fire("AddOutput", "OnEntitySpawned citizen_b_regular_original,SetDamageFilter,filter_player,0.1,-1")
         end)
 
         ents.WaitForEntityByName("rocketman", function(ent)
-            ent:SetHealth(100)
-        end)
-
-        ents.WaitForEntityByName("gatekeeper", function(ent)
             ent:SetHealth(100)
         end)
 
@@ -78,6 +184,34 @@ function MAPSCRIPT:PostInit()
             return true -- Suppress
         end)
 
+        GAMEMODE:WaitForInput("gunship_spawner_2", "Spawn", function(ent)
+            ent:RemoveTemplateData("OnDeath")
+            ent:AddTemplateData("squadname", "lambda_gunships")
+            ent:SetKeyValue("SpawnFrequency", "10")
+            ent:Enable()
+
+            ent.OnAllSpawnedDead = function(e)
+                TriggerOutputs({{"ag_siren", "StopSound", 0.0, ""}, {"lr_radioloop", "Disable", 0.0, ""}, {"citizen_standoff", "Kill", 0.0, ""}, {"aigf_combat", "Kill", 0.0, ""}, {"aisc_odessapostgunship", "Enable", 0.0, ""}, {"lr_squad_follow_*", "Kill", 0.0, ""}, {"post_gunship_jeep_relay*", "Enable", 0.0, ""}, {"aigf_odessapostgunship*", "Activate", 0.10, ""}, {"ss_post**", "BeginSequence", 2.00, ""}, {"gunshipdown_music*", "PlaySound", 3.00, ""}})
+            end
+
+            return true
+        end)
+
+        ents.WaitForEntityByName("gatekeeper", function(ent)
+            ent:SetHealth(100)
+            ent:Fire("SetDamageFilter", "filter_player")
+        end)
+
+        GAMEMODE:WaitForInput("village_gate", "Open", function(gate_ent)
+            ents.WaitForEntityByName("gatekeeper", function(ent)
+                GAMEMODE:UnregisterMissionCriticalNPC(ent)
+            end)
+
+            ents.WaitForEntityByName("road_exploit_block", function(ent)
+                ent:SetCollisionGroup(COLLISION_GROUP_VEHICLE_CLIP)
+            end)
+        end)
+
         local checkpoint1 = GAMEMODE:CreateCheckpoint(Vector(-5971.663574, 3534.091064, 269.338867), Angle(4.653, 55.612, 0.000))
 
         GAMEMODE:WaitForInput("spypost_template", "ForceSpawn", function(ent)
@@ -91,19 +225,6 @@ function MAPSCRIPT:PostInit()
         GAMEMODE:WaitForInput("aisc_pre_ingreeterrange", "Enable", function(ent)
             GAMEMODE:SetPlayerCheckpoint(checkpoint2)
             GAMEMODE:SetVehicleCheckpoint(Vector(6610.592285, 4405.477539, 264.207794), Angle(0.091, -121.466, 0.363))
-        end)
-
-        GAMEMODE:WaitForInput("gunship_spawner_2", "Spawn", function(ent)
-            ent:RemoveTemplateData("OnDeath")
-            ent:AddTemplateData("squadname", "lambda_gunships")
-            ent:SetKeyValue("SpawnFrequency", "10")
-            ent:Enable()
-
-            ent.OnAllSpawnedDead = function(e)
-                TriggerOutputs({{"ag_siren", "StopSound", 0.0, ""}, {"lr_radioloop", "Disable", 0.0, ""}, {"citizen_standoff", "Kill", 0.0, ""}, {"aigf_combat", "Kill", 0.0, ""}, {"aisc_odessapostgunship", "Enable", 0.0, ""}, {"lr_squad_follow_*", "Kill", 0.0, ""}, {"post_gunship_jeep_relay*", "Enable", 0.0, ""}, {"aigf_odessapostgunship*", "Activate", 0.10, ""}, {"ss_post**", "BeginSequence", 2.00, ""}, {"gunshipdown_music*", "PlaySound", 3.00, ""}})
-            end
-
-            return true
         end)
     end
 end
