f79fd675f829844d291316a8d69712b677a392df
knoxed <knoxed3@gmail.com>
2024-03-23 09:54:42 +0100

    Add 2 checkpoints and rpg to map loadout on c17_06

--- gamemode/gametypes/hl2ep1/mapscripts/ep1_c17_06.lua
+++ gamemode/gametypes/hl2ep1/mapscripts/ep1_c17_06.lua
@@ -4,7 +4,7 @@ end
 
 local MAPSCRIPT = {}
 MAPSCRIPT.DefaultLoadout = {
-    Weapons = {"weapon_crowbar", "weapon_physcannon", "weapon_pistol", "weapon_smg1", "weapon_357", "weapon_shotgun", "weapon_frag", "weapon_ar2", "weapon_crossbow"},
+    Weapons = {"weapon_crowbar", "weapon_physcannon", "weapon_pistol", "weapon_smg1", "weapon_357", "weapon_shotgun", "weapon_frag", "weapon_ar2", "weapon_rpg", "weapon_crossbow"},
     Ammo = {
         ["Pistol"] = 18,
         ["SMG1"] = 45,
@@ -13,7 +13,8 @@ MAPSCRIPT.DefaultLoadout = {
         ["Grenade"] = 3,
         ["AR2"] = 50,
         ["SMG1_Grenade"] = 1,
-        ["XBowBolt"] = 4
+        ["XBowBolt"] = 4,
+        ["RPG_Round"] = 3,
     },
     Armor = 0,
     HEV = true
@@ -39,10 +40,27 @@ MAPSCRIPT.GlobalStates = {
 }
 
 MAPSCRIPT.Checkpoints = {
+    {
+        Pos = Vector(12302, 9626, -728),
+        Ang = Angle(0, 90, 0),
+        Trigger = {
+            Pos = Vector(12308, 9405, -678),
+            Mins = Vector(-68, -19, -54),
+            Maxs = Vector(68, 19, 54)
+        }
+    },
+    {
+        Pos = Vector(11934, 8531, -728),
+        Ang = Angle(0, 90, 0),
+        Trigger = {
+            Pos = Vector(11946, 8528, -696),
+            Mins = Vector(-101, -64, -40),
+            Maxs = Vector(101, 64, 40)
+        }
+    },
 }
 
 function MAPSCRIPT:PostInit()
-
     -- Replace the train start trigger with a bigger one and a teamwait keyvalue and create a checkpoint parented to the train
     local trainCheckpoint = GAMEMODE:CreateCheckpoint(Vector(11986, 8409, -759), Angle(0, 90, 0))
 
