c625eaa5facdc6fb6e1b7d1a9d0dfe64dc7acc7c
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-05-26 00:05:40 +0300

    Start ep2_outland_01a, handle the elevator

--- gamemode/gametypes/hl2ep2/mapscripts/ep2_outland_01a.lua
+++ gamemode/gametypes/hl2ep2/mapscripts/ep2_outland_01a.lua
@@ -22,7 +22,31 @@ MAPSCRIPT.Checkpoints = {
 }
 
 function MAPSCRIPT:PostInit()
-    print("-- Incomplete mapscript --")
+    -- Apply enough force to do more than one player.
+    ents.WaitForEntityByName("platform_2_thruster_1", function(ent)
+        ent:SetKeyValue("force", "1000")
+    end)
+
+    -- This is one ugly hack, we want to keep the velocity of the platform in check due to the force applied.
+    -- Without enough force it won't budge and with too much force it sends players flying.
+    ents.WaitForEntityByName("platform_2", function(ent)
+        hook.Add("Think", "PlatformVelocityControl", function()
+            if not IsValid(ent) then
+                return
+            end
+            local physObj = ent:GetPhysicsObject()
+            if not IsValid(physObj) then
+                return
+            end
+            local vel = physObj:GetVelocity()
+            local len = vel:LengthSqr()
+            local maxSpeed = 150
+            local maxLen = maxSpeed * maxSpeed
+            if len > maxLen then
+                physObj:SetVelocity(vel:GetNormalized() * maxSpeed)
+            end
+        end)
+    end)
 end
 
 return MAPSCRIPT
