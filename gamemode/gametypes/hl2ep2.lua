44e9d2a99c434435c7553a81e2535fcdcb287551
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-06-02 14:57:21 +0300

    Use correct map order

--- gamemode/gametypes/hl2ep2.lua
+++ gamemode/gametypes/hl2ep2.lua
@@ -13,6 +13,7 @@ GAMETYPE.MapList = {
     "ep2_outland_02",
     "ep2_outland_03",
     "ep2_outland_04",
+    "ep2_outland_02",
     "ep2_outland_05",
     "ep2_outland_06",
     "ep2_outland_06a",
