78f3a4a5f958bd9a1b4271d40035df556c8e1b30
knoxed <knoxed3@gmail.com>
2024-06-23 18:30:16 +0200

    06 mapscript improvement

--- gamemode/gametypes/hl2ep2/mapscripts/ep2_outland_06.lua
+++ gamemode/gametypes/hl2ep2/mapscripts/ep2_outland_06.lua
@@ -16,8 +16,11 @@ MAPSCRIPT.DefaultLoadout = {
     },
     Ammo = {
         ["SMG1"] = 45,
-        ["Grenade"] = 5,
-        ["Pistol"] = 20,
+        ["Buckshot"] = 6,
+        ["Pistol"] = 18,
+        ["Grenade"] = 3,
+        ["357"] = 6,
+        ["SMG1_Grenade"] = 1,
     },
     Armor = 15,
     HEV = true
@@ -26,8 +29,10 @@ MAPSCRIPT.DefaultLoadout = {
 MAPSCRIPT.InputFilters = {}
 MAPSCRIPT.EntityFilterByClass = {}
 MAPSCRIPT.EntityFilterByName = {
-    -- FIXME: Contains alyx and vortigaunt.
-    --["global_newgame_template_base_items"] = true,
+    ["global_newgame_spawner_suit"] = true,
+    ["global_newgame_spawner_crowbar"] = true,
+    ["global_newgame_spawner_pistol"] = true,
+    ["global_newgame_spawner_physcannon"] = true,
     ["global_newgame_template_ammo"] = true,
     ["global_newgame_template_local_items"] = true,
 }
@@ -36,10 +41,92 @@ MAPSCRIPT.GlobalStates = {
 }
 
 MAPSCRIPT.Checkpoints = {
+    {
+        Pos = Vector(737, 480, 101), -- 2
+        Ang = Angle(0, 0, 0),
+        Trigger = {
+            Pos = Vector(600, 506, 156),
+            Mins = Vector(-4, -56, -60),
+            Maxs = Vector(4, 56, 60)
+        }
+    },
+    {
+        Pos = Vector(731, 2040, 101), -- 4
+        Ang = Angle(0, 0, 0),
+        Trigger = {
+            Pos = Vector(728, 1988, 128),
+            Mins = Vector(-40, -60, -40),
+            Maxs = Vector(40, 60, 40)
+        }
+    },
+    {
+        Pos = Vector(2848, 1358, -280), -- 8
+        Ang = Angle(0, 0, 0),
+        Trigger = {
+            Pos = Vector(2873, 1314, -258),
+            Mins = Vector(-42, -122, -16),
+            Maxs = Vector(42, 122, 16)
+        }
+    },
 }
 
 function MAPSCRIPT:PostInit()
-    print("-- Incomplete mapscript --")
+    ents.WaitForEntityByName("trigger_alyxComeDownToJeep", function(ent)
+        ent.OnTrigger = function()
+            local loadout = GAMEMODE:GetMapScript().DefaultLoadout
+            table.insert(loadout.Weapons, "weapon_ar2")
+        end
+    end)
+
+    -- triggers actually have names on this map, lets make use of that for checkpoints
+    local cp1 = GAMEMODE:CreateCheckpoint(Vector(331, 506, 785))
+    ents.WaitForEntityByName("bldg1_trigger", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp1, activator)
+        end
+    end)
+
+    local cp3 = GAMEMODE:CreateCheckpoint(Vector(1874, 1079, -123))
+    ents.WaitForEntityByName("alyx_sniperGuide00", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp3, activator)
+        end
+    end)
+
+    local cp5 = GAMEMODE:CreateCheckpoint(Vector(22, 3296, -114))
+    ents.WaitForEntityByName("trigger_warehouse_noisesInTheDark", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp5, activator)
+        end
+    end)
+
+    local cp6 = GAMEMODE:CreateCheckpoint(Vector(1118, 2798, -252))
+    ents.WaitForEntityByName("trigger_startspawn_goopit1", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp6, activator)
+        end
+    end)
+
+    local cp7 = GAMEMODE:CreateCheckpoint(Vector(1904, 2032, -282))
+    ents.WaitForEntityByName("zombieTrigger_topofPipeHeadCrabs", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp7, activator)
+        end
+    end)
+
+    local cp9 = GAMEMODE:CreateCheckpoint(Vector(3961, 2340, 637))
+    ents.WaitForEntityByName("jeep_autosave", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp9, activator)
+        end
+    end)
+
+    local cp10 = GAMEMODE:CreateCheckpoint(Vector(-400, 2344, 693))
+    ents.WaitForEntityByName("trigger_alyxComeDownToJeep", function(ent)
+        ent.OnTrigger = function(_, activator)
+            GAMEMODE:SetPlayerCheckpoint(cp10, activator)
+        end
+    end)
 end
 
 return MAPSCRIPT
