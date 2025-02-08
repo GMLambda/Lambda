f662c66b379e336ce3ee33fc74a82116ee015ff6
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-02-09 00:32:40 +0200

    Implement ep2_outland_06a

--- gamemode/gametypes/hl2ep2/mapscripts/ep2_outland_06a.lua
+++ gamemode/gametypes/hl2ep2/mapscripts/ep2_outland_06a.lua
@@ -34,17 +34,32 @@ MAPSCRIPT.EntityFilterByName = {
     ["global_newgame_template_base_items"] = true,
     ["global_newgame_template_local_items"] = true,
     ["playerclip_powerroom"] = true,
+    ["playerclip_elevator"] = true,
+    ["teleport_jeep"] = true,
+    ["fade_start"] = true,
 }
 
 MAPSCRIPT.GlobalStates = {
 }
 
 MAPSCRIPT.Checkpoints = {
+    {
+        Pos = Vector(-1763.0, -9607.42, -1536),
+        Ang = Angle(0, 109, 0),
+        RenderPos = Vector(-1814.464355, -9021.433594, -1556.093262),
+        Trigger = {
+            Pos = Vector(-1312.065430, -9076.086914, -1576.651123),
+            Mins = Vector(-100, -100, 0),
+            Maxs = Vector(100, 100, 100)
+        },
+        Vehicle = {
+            Pos = Vector(-1724.15, -9506.5, -1533),
+            Ang = Angle(0, 40, 0),
+        },
+    },
 }
 
 function MAPSCRIPT:PostInit()
-    print("-- Incomplete mapscript --")
-
     -- Power room checkpoint
     local cp1_powerroom = GAMEMODE:CreateCheckpoint(Vector(-3330, -9731, -1519))
     ents.WaitForEntityByName("trigger_alyx_standby", function(ent)
