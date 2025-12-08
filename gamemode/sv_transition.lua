91590b033e70cf233b5fb62d4bfe692edd1efa47
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-12-08 19:51:11 +0200

    Fix a weird bug

--- gamemode/sv_transition.lua
+++ gamemode/sv_transition.lua
@@ -921,12 +921,11 @@ function GM:CreateTransitionObjects()
                 DbgPrint("Using global entity!")
             else
                 ent = ents.Create(data.Class)
+                if not IsValid(ent) then
+                    DbgPrint("Failed to create entity of class: " .. data.Class)
+                    continue
+                end
                 ent.CreatedByLevelTransition = true
-            end
-
-            if not IsValid(ent) then
-                DbgPrint("Attempted to create bogus entity: " .. data.Class)
-                continue
             end
 
             ent.SourceMap = data.SourceMap
