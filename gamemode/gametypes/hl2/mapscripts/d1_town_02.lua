e66cadc747f06422bef5f0912efa5faf8619157e
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-12-14 20:56:55 +0200

    Improve anti rush on d1_town_02

--- gamemode/gametypes/hl2/mapscripts/d1_town_02.lua
+++ gamemode/gametypes/hl2/mapscripts/d1_town_02.lua
@@ -91,6 +91,33 @@ function MAPSCRIPT:PostInit()
 
                 return true -- Suppress.
             end)
+
+            -- Anti rush.
+            -- -3190.385254 508.148438 -3583.968750
+            local antiRush01 = ents.Create("trigger_hurt")
+            antiRush01:SetupTrigger(Vector(-3190.385254, 508.148438, -3583.968750),
+                Angle(0, 0, 0),
+                Vector(-300, -500, 0),
+                Vector(330, 300, 135))
+            antiRush01:SetKeyValue("damagetype", tostring(DMG_FALL))
+            antiRush01:SetKeyValue("damage", "2000")
+            antiRush01:SetName("lambda_anti_rush_01")
+
+            local antiRush01Done = ents.Create("trigger_once")
+            antiRush01Done:SetupTrigger(Vector(-2935, 828, -3516),
+                Angle(0, 0, 0),
+                Vector(-60, -20, 0),
+                Vector(60, 20, 120))
+            antiRush01Done:Fire("AddOutput", "OnTrigger lambda_anti_rush_01,Kill,,0,-1")
+
+            -- Block another spot where players could skip a large part.
+            local playerBlock = ents.Create("func_brush")
+            playerBlock:SetPos(Vector(-3363.836670, 651.215271, -3196))
+            playerBlock:SetModel("*71")
+            -- Let all NPCs pass but not players.
+            playerBlock:SetKeyValue("invert_exclusion", "1")
+            playerBlock:Spawn()
+
         else
             -- We spawn the monk in the second part sooner, players should not see him being spawned.
             -- -3396.734619 417.609131 -3327.968750
